bb4395f4295f5b368b4740aaa138d0c6c8308ee0
TinCodeX <tinsaebefekadu2012@gmail.com>
2026-05-02 19:34:40 +0300

    api swap

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://vm-mma-backend.onrender.com',
 });
 
 // Request interceptor to add the auth token header to every request
